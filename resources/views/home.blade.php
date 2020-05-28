6db71cc5815c0b3d9030edfb68b1d59387a2fb0c
LucianoMLujan <lucianomlujan@gmail.com>
2020-05-28 10:10:31 -0300

    Add image list on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,23 @@
         <div class="col-md-8">
             @include('includes.message')
             
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            @foreach($images as $image)
+                <div class="card pub_image">
+                    <div class="card-header">
+                        @if($image->user->image)
+                            <div class="container-avatar">
+                                <img src="{{ route('user.avatar', ['filename'=>$image->user->image]) }}" alt="" class="avatar">
+                            </div>
+                        @endif
+                        <div class="data-user">
+                            {{$image->user->nick}}
                         </div>
-                    @endif
+                    </div>
 
-                    You are logged in!
+                    <div class="card-body">
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
